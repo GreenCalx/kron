ff1a5880de3c21c746f0899ccec4ae31226498cf
GreenCalx <romain.schoenberger@gmail.com>
2024-09-06 12:42:05 +0200

    talkative npc wip

---
+++ KRON/Assets/Scripts/TalkativeNPC.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkativeNPC : MonoBehaviour
+{
+    public Animation animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!!animation)
+            animation.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
