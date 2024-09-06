b7f764ecff9b3396f0317e4e8f0fbe3ab3e280ca
GreenCalx <romain.schoenberger@gmail.com>
2024-09-06 13:18:05 +0200

    talkative npc with prefab

--- KRON/Assets/Scripts/TalkativeNPC.cs
+++ KRON/Assets/Scripts/TalkativeNPC.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class TalkativeNPC : MonoBehaviour
 {
-    public Animation animation;
+    [Header("Animation")]
+    public Image uiImageTarget;
+    public List<Sprite> spritesToAnimate;
+    public float animFrameDuration = 0.5f;
+    public bool animateBubble = true;
+
+    private Coroutine animateBubbleCo;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!!animation)
-            animation.Play();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (animateBubble && (animateBubbleCo==null))
+        {
+            StartBubbleAnim();
+        }
+    }
+
+    private void StartBubbleAnim()
+    {
+        uiImageTarget.sprite = spritesToAnimate[0];
+        animateBubbleCo = StartCoroutine(BubbleAnimCo());
+
+        uiImageTarget.gameObject.SetActive(true);
+    }
+
+    private void StopBubbleAnim()
+    {
+        StopCoroutine(animateBubbleCo);
+        animateBubbleCo = null;
+
+        uiImageTarget.gameObject.SetActive(false);
+    }
+
+    IEnumerator BubbleAnimCo()
+    {
+        int spriteIndex = 0;
+        while (animateBubble)
+        {
+            uiImageTarget.sprite = spritesToAnimate[spriteIndex];
+            spriteIndex++;
+            if (spriteIndex >= spritesToAnimate.Count)
+                spriteIndex = 0;
+            yield return new WaitForSeconds(animFrameDuration);
+        }
     }
 }
